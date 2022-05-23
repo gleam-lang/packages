3c11b2c8754887cb2f87f3f8e0eb771a4845d600
Harry Bairstow <h.bairstow22@gmail.com>
2022-05-23 13:15:40 +0100

    Remove ffi

--- src/packages_ffi.erl
+++
@@ -1,8 +0,0 @@
--module(packages_ffi).
-
--export([to_gregorian_seconds/1]).
-
-to_gregorian_seconds({{Year, Month, Day}, {Hours, Minutes, Seconds}}) ->
-    calendar:datetime_to_gregorian_seconds(
-        {{Year, Month, Day}, {Hours, Minutes, Seconds}}
-    ).
