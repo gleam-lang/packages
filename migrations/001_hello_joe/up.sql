c7c4e7e65ebf1c5b8ea6315a228e57f36042c3a7
Harry Bairstow <h.bairstow22@gmail.com>
2022-10-16 17:35:21 +0100

    fix: Migration

--- migrations/001_hello_joe/up.sql
+++ migrations/001_hello_joe/up.sql
@@ -1,17 +1,19 @@
-create table packages (
-  name varchar(100) primary key,
-  updated_at timestamp not null default current_timestamp,
-  imported_at timestamp not null default current_timestamp,
-  links jsonb not null default '{}'::jsonb,
-  licenses varchar(255)[] not null default '[]',
-  description not null text default 'My Description' 
+create table packages
+(
+    name        varchar(100) primary key,
+    updated_at  timestamp      not null default current_timestamp,
+    imported_at timestamp      not null default current_timestamp,
+    links       jsonb          not null default '{}'::jsonb,
+    licenses    varchar(255)[] not null default '[]',
+    description text           not null default 'My Description'
 );
 
-create table previous_hex_api_scan (
-  id boolean primary key default true,
-  scanned_at timestamp not null,
+create table previous_hex_api_scan
+(
+    id         boolean primary key default true,
+    scanned_at timestamp not null,
 
-  -- We use a constraint to enforce that the id is always the value `true` so
-  -- now this table can only hold one row.
-  constraint previous_hex_api_scan_singleton check (id)
+    -- We use a constraint to enforce that the id is always the value `true` so
+    -- now this table can only hold one row.
+    constraint previous_hex_api_scan_singleton check (id)
 )
