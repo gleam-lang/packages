7f997bbc2a5bc4abeb0ad410f1f17f58485ddc74
Louis Pilfold <louis@lpil.uk>
2023-04-14 17:51:40 +0100

    PostgreSQL schema

---
+++ sql/schema.sql
@@ -0,0 +1,47 @@
+create table if not exists packages
+( id serial primary key
+, name text not null unique
+, hex_html_url text
+, docs_html_url text
+, inserted_in_hex_at timestamp with time zone
+, updated_in_hex_at timestamp with time zone
+, links jsonb not null default '{}'
+, licenses text array not null default '{}'
+, description text
+);
+
+create table if not exists hex_user
+( id serial primary key
+, username text not null unique
+, email text
+, hex_url text
+);
+
+create table if not exists package_ownership
+( package_id integer references packages(id) on delete cascade
+, hex_user_id integer references hex_user(id) on delete cascade
+, primary key (package_id, hex_user_id)
+);
+
+do $$
+begin
+  if to_regtype('retirement_reason') is null then
+    create type retirement_reason as enum
+    ( 'other'
+    , 'invalid'
+    , 'security'
+    , 'deprecated'
+    , 'renamed'
+    );
+  end if;
+end
+$$;
+
+create table if not exists releases
+( id serial primary key
+, package_id integer references packages(id) on delete cascade
+, version text not null
+, hex_url text not null
+, retirement_reason retirement_reason
+, retirement_message text
+);
